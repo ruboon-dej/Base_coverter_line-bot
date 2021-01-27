e4da6643684f0fe59bb7849451bd956568013677
ruboon-dej <ruboon_d@yahoo.com>
2021-01-27 18:34:39 +0700

    finish I hope

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from baseconverter.converter import convert
 from flask import Flask, request, abort
 from flask_caching import Cache
 
@@ -14,7 +13,34 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+from user import User
+
+cache = Cache()
 app = Flask(__name__)
+
+cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+cache.init_app(app,
+    config={'CACHE_TYPE': 'saslmemcached',
+            'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+            'CACHE_MEMCACHED_USERNAME': cache_user,
+            'CACHE_MEMCACHED_PASSWORD': cache_pass,
+            'CACHE_OPTIONS': { 'behaviors': {
+                # Faster IO
+                'tcp_nodelay': True,
+                # Keep connection alive
+                'tcp_keepalive': True,
+                # Timeout for set/get requests
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30}}})
 
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
@@ -40,9 +66,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_id = event.source.user_id
+
+    user = cache.get(userId)
+    if user is None:
+        user = User()
+        cache.set(userId, user)
+
+    text = user.get_response(event.message.text)
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=text))
 
 
 if __name__ == "__main__":
---
+++ user.py
@@ -0,0 +1,38 @@
+from baseconverter.converter import convert
+
+ASK_FOR_ORIGINAL_BASE = "What is the original base?"
+ASK_FOR_TARGET_BASE = "What is the target base?"
+ASK_FOR_VALUE = "What is the value ?"
+
+class User:
+    def __init__(self):
+        self.original_base = None
+        self.target_base = None
+        self.value = None
+    
+    def get_response(self, text):
+        if self.original_base is None:
+            if text.isdigit():
+                self.original_base = int(text)
+                return ASK_FOR_TARGET_BASE
+            else:
+                return ASK_FOR_ORIGINAL_BASE
+
+
+        elif self.target_base is None:
+            if text.isdigit():
+                self.target_base = int(text)
+                return ASK_FOR_VALUE
+            else:
+                return ASK_FOR_TARGET_BASE
+
+
+        elif self.value is None:
+            if text.isdigit():
+                self.value = int(text)
+                return self.calulate_answer()
+            else:
+                return ASK_FOR_VALUE
+
+    def calculate_answer(self):
+        return convert(self.original_base, self.target_base, self.value)
