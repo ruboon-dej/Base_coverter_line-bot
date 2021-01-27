673ceee0b5408591b6b4f22358a9920597457db4
ruboon-dej <ruboon_d@yahoo.com>
2021-01-27 16:53:52 +0700

    fixed bug

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
 
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -45,6 +44,4 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
-if __name__ == "__main__":
-    app.run()
+app.run()
